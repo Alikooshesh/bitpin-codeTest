067408c6ebd4e8d2577e3772a85e67ff004a8c6e
Ali Kooshesh <alikooshesh007@gmail.com>
2022-04-29 15:48:21 +0430

    feat(PaginationMarketCard): add PaginationMarketCard

---
+++ src/components/paginationMarketCard/index.tsx
@@ -0,0 +1,57 @@
+import {ImarketCard} from "../../interface/global";
+import {useEffect, useState} from "react";
+import MarketCard from "../cards/marketCard";
+import {useSelector} from "react-redux";
+import {ArrowLeft2, ArrowRight2} from "iconsax-react";
+
+interface props{
+    cardsInfo : ImarketCard[]
+}
+
+const PaginationMarketCard = ({cardsInfo}:props) => {
+
+    const favMarkets = useSelector((state:any) => state.favMarket.favMarkets)
+
+    const [page , setPage] = useState<number>(1)
+    const [pagesCount , setPagesCount] = useState<number|null>(null)
+
+    useEffect(()=>{
+        const divisBy10 = cardsInfo.length % 10
+        setPagesCount(divisBy10 ? cardsInfo.length / 10 : (cardsInfo.length / 10)+1)
+    },[cardsInfo])
+
+    return(
+        <>
+            <div className={'flex justify-center gap-8 flex-wrap'}>
+                {cardsInfo.filter((market , index) => {
+                    if ((index >= ((page * 10) - 10)) && (index < (page * 10))){
+                        return market
+                    }
+                }).map((market) => <MarketCard
+                    key={`marketCard-${market.title}-${market.code}`}
+                    currenyImages={market.currenyImages} price={market.price}
+                    priceUnit={market.priceUnit} title={market.title}
+                    code={market.code}
+                    isFav={favMarkets?.includes(market.code)}
+                />)}
+            </div>
+
+            <div className={'mt-16 flex justify-center items-center gap-8'}>
+                <ArrowLeft2
+                    size="32"
+                    color={page !== 1 ? "#FF8A65" : "#545252"}
+                    onClick={()=> page !== 1 && setPage(page-1)}
+                    className={page !== 1 ? "cursor-pointer" : ""}
+                />
+                <p>{page}</p>
+                <ArrowRight2
+                    size="32"
+                    color={page !== pagesCount ? "#FF8A65" : "#545252"}
+                    onClick={()=> page !== pagesCount && setPage(page+1)}
+                    className={page !== pagesCount ? "cursor-pointer" : ""}/>
+            </div>
+        </>
+    )
+}
+
+export default PaginationMarketCard
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,66 +3,59 @@ import {useEffect, useState} from "react";
 import {ImarketCard} from "../../interface/global";
 import axios from 'axios'
 import {useSelector} from "react-redux";
+import PaginationMarketCard from "../../components/paginationMarketCard";
 
 const Home = () => {
 
-    const favMarkets = useSelector((state:any) => state.favMarket.favMarkets)
+    const favMarkets = useSelector((state: any) => state.favMarket.favMarkets)
 
-    const [isLoading , setIsloading] = useState<boolean>(false)
+    const [isLoading, setIsloading] = useState<boolean>(false)
 
-    const [markets , setMarkets] = useState<ImarketCard[] | null>(null)
+    const [markets, setMarkets] = useState<ImarketCard[] | null>(null)
 
-    const [showFavList , setShowFavList] = useState<boolean>(false)
+    const [showFavList, setShowFavList] = useState<boolean>(false)
 
-    useEffect(()=>{
+    useEffect(() => {
         setIsloading(true)
         axios.get('https://api.bitpin.ir/v1/mkt/markets/')
             .then(data => {
                 setIsloading(false)
                 const fetched = data.data.results
-                const marketList:ImarketCard[] = []
-                fetched.forEach((market:any) => {
-                    const newMarket:ImarketCard = {
-                        currenyImages : [market.currency1.image , market.currency2.image],
-                        price : market.price,
-                        priceUnit : market.currency2.title ,
-                        title : market.title,
-                        code : market.code,
+                const marketList: ImarketCard[] = []
+                fetched.forEach((market: any) => {
+                    const newMarket: ImarketCard = {
+                        currenyImages: [market.currency1.image, market.currency2.image],
+                        price: market.price,
+                        priceUnit: market.currency2.title,
+                        title: market.title,
+                        code: market.code,
                     }
                     marketList.push(newMarket)
                 })
                 setMarkets(marketList)
             })
-    },[])
+    }, [])
 
-    return(
+    return (
         <div className={'p-16 flex flex-col items-center justify-center'}>
             {isLoading && <h1>Is Loading ...</h1>}
             {!isLoading && !markets?.length && <h1>Data is empty</h1>}
             {!isLoading && markets?.length &&
             <>
                 <button
-                    style={{backgroundColor : 'cadetblue' , color : 'white' , fontSize : '2rem'}}
+                    style={{backgroundColor: 'cadetblue', color: 'white', fontSize: '2rem'}}
                     className={'p-16 mx-auto cursor-pointer'}
-                    onClick={()=> setShowFavList(!showFavList)}>
+                    onClick={() => setShowFavList(!showFavList)}>
                     {showFavList ? 'show all markets' : 'show fav markets'}
                 </button>
 
-                <div className={'mt-8 flex justify-center gap-8 flex-wrap'}>
-                    {markets?.filter(market => favMarkets?.includes(market.code)).map(market => <MarketCard
-                        key={`marketCard-${market.title}-${market.code}`}
-                        currenyImages={market.currenyImages} price={market.price}
-                        priceUnit={market.priceUnit} title={market.title}
-                        code={market.code}
-                        isFav={favMarkets?.includes(market.code)}
-                    />)}
-                    {!showFavList && markets?.filter(market => !favMarkets?.includes(market.code)).map(market => <MarketCard
-                        key={`marketCard-${market.title}-${market.code}`}
-                        currenyImages={market.currenyImages} price={market.price}
-                        priceUnit={market.priceUnit} title={market.title}
-                        code={market.code}
-                        isFav={favMarkets?.includes(market.code)}
-                    />)}
+                <div className={'mt-8'}>
+                    <PaginationMarketCard cardsInfo={showFavList ?
+                        [...markets?.filter(market => favMarkets?.includes(market.code))] :
+                        [
+                            ...markets?.filter(market => favMarkets?.includes(market.code)),
+                            ...markets?.filter(market => !favMarkets?.includes(market.code))
+                        ]}/>
                 </div>
             </>}
 
